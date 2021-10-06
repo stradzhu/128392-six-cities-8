a2d733fa2eda50495769747312dcd152dffe8633
Dmitriy Stradzhu (home) <stradzhu.d@gmail.com>
2021-10-06 15:37:42 +0300

    создал jsx компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  COUNT_RENTAL_OFFERS: 789,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App countRentalOffers={Setting.COUNT_RENTAL_OFFERS} />
   </React.StrictMode>,
   document.getElementById('root'));
