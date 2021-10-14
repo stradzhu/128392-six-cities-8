da6a79b370a7b985dcae9bf7214c118a42d7c297
Dmitriy Stradzhu (home) <stradzhu.d@gmail.com>
2021-10-14 13:04:28 +0300

    перенес компоненты внутрь файла, т.к. пока они используются только 1 раз, то, мне кажется, и выносить нет смысла

--- project/src/components/main/main.tsx
+++ project/src/components/main/main.tsx
@@ -1,9 +1,45 @@
-import CitiesPlaceCard from '../cities-place-card/cities-place-card';
+import {Link} from 'react-router-dom';
 import Header from '../header/header';
 import Locations from '../locations/locations';
 
 type MainProps = {
   countRentalOffers: number
+}
+
+function CitiesPlaceCard(): JSX.Element {
+  return (
+    <article className="cities__place-card place-card">
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <Link to="/">
+          <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="" />
+        </Link>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;80</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark" />
+            </svg>
+            <span className="visually-hidden">In bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{'width': '80%'}} />
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <Link to="/">Wood and stone place</Link>
+        </h2>
+        <p className="place-card__type">Private room</p>
+      </div>
+    </article>
+  );
 }
 
 function Main({countRentalOffers}: MainProps): JSX.Element {
